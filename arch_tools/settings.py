975a97f4d2fb5928421d95928d75581016d7259f
oncampus.teamgh@gmail.com <oncampus.teamgh@gmail.com>
2025-01-16 17:25:55 +0000

    Deploying to Render

--- arch_tools/settings.py
+++ arch_tools/settings.py
@@ -121,6 +121,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+# MEDIA_URL = '/img/portfolio/'  #link display in browser when the media is opened separately
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/assets/media/") 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
